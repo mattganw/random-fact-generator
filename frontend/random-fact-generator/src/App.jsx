582300f08bd3f1652e319bc745b766cc85933632
Matthew Wang <mattchsw@gmail.com>
2026-01-28 21:38:01 -0500

    add fetch request

--- frontend/random-fact-generator/src/App.jsx
+++ frontend/random-fact-generator/src/App.jsx
@@ -1,15 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
-import "./components/FactCard.jsx"
 import FactCard from './components/FactCard.jsx'
 
 function App() {
 
+  const [fact, setFact] = useState(null)
+
+  const getFact = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/api/fact")
+      const data = await response.json()
+      setFact(data)
+
+    } catch(err) {
+      console.error("failed to fetch fect: ", err)
+    }
+  }  
+
+  useEffect(() => {
+    getFact()
+
+  }, [])
+
   return (
     <>
-      <FactCard />
+      {fact ? fact.text : "Loading..."}
     </>
   )
 }
