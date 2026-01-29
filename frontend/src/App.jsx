b91ef7eab0ebc6c7d1179caba5f0c60fd471c3a5
MATT <MATT@WANGM2.SCDA.COUNTY.SUF>
2026-01-29 12:12:37 -0500

    add loading and error states

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,26 +5,41 @@ import FactCard from './components/FactCard.jsx'
 function App() {
 
   const [fact, setFact] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
 
   const getFact = async () => {
     try {
+      setLoading(true)
+      setError(null)
+
       const response = await fetch("http://localhost:3000/api/fact")
+
+      if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`)
+      }
+
       const data = await response.json()
       setFact(data)
 
     } catch(err) {
+      setError(err)
       console.error("failed to fetch fect: ", err)
+    } finally {
+      setLoading(false)
     }
   }  
 
   useEffect(() => {
     getFact()
-
   }, [])
+
+  if (loading) return <>Loading...</>
+  if (error) return <>Error: {error.message}</>
 
   return (
     <>
-      {fact ? fact.text : "Loading..."}
+      {fact ? fact.text : "No fact available"}
     </>
   )
 }
