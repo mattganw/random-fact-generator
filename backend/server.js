64b2bb80768b4f1c6262fc94090552a603932487
Matthew Wang <mattchsw@gmail.com>
2026-01-28 20:36:35 -0500

    add first route

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,17 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/api/fact", async (req, res) => {
+    try {
+        const response = await fetch("https://uselessfacts.jsph.pl/api/v2/facts/random")
+        const data = await response.json();
+
+        res.json(data)
+    } catch (err) {
+        console.log(err)
+    }
+})
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
 })
