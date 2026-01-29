eb4189e856d9a67bd8ab9974c4c1e1f0647d945c
Matthew Wang <mattchsw@gmail.com>
2026-01-28 20:46:14 -0500

    adding route for today's fact, appended language to endpoints

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,22 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 
+// get a random fact
 app.get("/api/fact", async (req, res) => {
     try {
-        const response = await fetch("https://uselessfacts.jsph.pl/api/v2/facts/random")
+        const response = await fetch("https://uselessfacts.jsph.pl/api/v2/facts/random?language=en")
+        const data = await response.json();
+
+        res.json(data)
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+// get today's fact
+app.get("/api/today", async (req, res) => {
+    try {
+        const response = await fetch("https://uselessfacts.jsph.pl/api/v2/facts/today?language=en")
         const data = await response.json();
 
         res.json(data)
