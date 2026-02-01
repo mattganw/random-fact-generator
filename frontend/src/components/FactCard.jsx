81a72b2cb19ef74c3cea826e83a2843e3013f576
Matthew Wang <mattchsw@gmail.com>
2026-02-01 17:09:50 -0500

    add button styles

--- frontend/src/components/FactCard.jsx
+++ frontend/src/components/FactCard.jsx
@@ -1,6 +1,8 @@
 import "../styles/FactCard.css"
 import LightbulbIcon from "./LightbulbIcon";
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import ReplayIcon from '@mui/icons-material/Replay';
+import SunnyIcon from '@mui/icons-material/Sunny';
 
 function FactCard({fact, onGetNewFact, onGetTodayFact}) {
     return (
@@ -12,13 +14,17 @@ function FactCard({fact, onGetNewFact, onGetTodayFact}) {
                         <h2 className="header-text google-sans-h2">Random Fact</h2>
                     </div>
                     <div className="card-header-right">
-                        <ContentCopyIcon />
+                        <ContentCopyIcon htmlColor="#8f8e8e"/>
                     </div>
                 </div>
                 <p className="fact-text google-sans-p">{fact[0].fact}</p>
                 <div className="card-footer">
-                    <button onClick={onGetNewFact}>Get New Fact</button>
-                    <button onClick={onGetTodayFact}>Get Today's Fact</button>
+                    <button className="new-fact-btn google-sans-btn"onClick={onGetNewFact}>
+                        <ReplayIcon htmlColor="#FFF"/> New Fact
+                    </button>
+                    <button className="today-fact-btn google-sans-btn" onClick={onGetTodayFact}>
+                        <SunnyIcon htmlColor="#FFCC33"/> Today's Fact
+                    </button>
                 </div>
             </div>
         </>
