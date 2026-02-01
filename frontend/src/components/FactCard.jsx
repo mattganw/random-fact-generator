4e83de5bc5bc66e056b74ddff3c7802a98c1d4ef
Matthew Wang <mattchsw@gmail.com>
2026-01-31 22:16:45 -0500

    adding icon

--- frontend/src/components/FactCard.jsx
+++ frontend/src/components/FactCard.jsx
@@ -1,10 +1,15 @@
 import "../styles/FactCard.css"
+import LightbulbIcon from "./LightbulbIcon";
 
 function FactCard({fact}) {
     return (
         <>
             <div className="card">
+                <div className="card-header">
+                    <LightbulbIcon />
+                </div>
                 <p className="fact-text google-sans-fact">{fact[0].fact}</p>
+                <div className="card-footer">Footer</div>
             </div>
         </>
     )
